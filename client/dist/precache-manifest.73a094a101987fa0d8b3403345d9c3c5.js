2fa8098ede51f3b6f69d72b4ed8cf16232cca901
supergczh <601140987@qq.com>
2019-08-17 15:40:23 +0800

    test

---
+++ client/dist/precache-manifest.73a094a101987fa0d8b3403345d9c3c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd09e4a33a09be8b77d",
+    "url": "/css/1.25f4e16181b22233a476.css"
+  },
+  {
+    "revision": "eb79b679b5172ec741141276a55dda72",
+    "url": "/css/1.25f4e16181b22233a476.css.gz"
+  },
+  {
+    "revision": "387c39980b660375184d66db7d0eea25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50c3c418dc255769e880",
+    "url": "/js/app-50c3c418.chunk.js"
+  },
+  {
+    "revision": "efd09e4a33a09be8b77d",
+    "url": "/js/vendor.efd09e4a.chunk.js"
+  },
+  {
+    "revision": "6c08404de04da6f0a11206fbf29d4a57",
+    "url": "/js/vendor.efd09e4a.chunk.js.gz"
+  },
+  {
+    "revision": "f862c9b177de81cc2f3ce46c1db1dfba",
+    "url": "/library_1143d988.dll.js"
+  },
+  {
+    "revision": "d3a6bd18f1f8d41c854b56a782db2cec",
+    "url": "/library_1143d988.dll.js.gz"
+  },
+  {
+    "revision": "aa1c6008ca9f9c3a4ada0708dfa5e0f1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d6760a249c02418e7d8fa0fff72af2ac",
+    "url": "/maniicon.ico"
+  }
+]);
